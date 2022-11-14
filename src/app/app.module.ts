36c45ff59db350243dfc549f445c3610112e3665
serraguti <pacoserranox@gmail.com>
2022-11-14 12:30:08 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {routing, appRoutingProviders} from './app.routing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CineComponent } from './components/cine/cine.component';
+import { MusicaComponent } from './components/musica/musica.component';
+import { TelevisionComponent } from './components/television/television.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { Error404Component } from './components/error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CineComponent,
+    MusicaComponent,
+    TelevisionComponent,
+    MenuComponent,
+    Error404Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule, routing
   ],
-  providers: [],
+  providers: [ appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,28 @@
+import { HomeComponent } from "./components/home/home.component";
+import { CineComponent } from "./components/cine/cine.component";
+import { MusicaComponent } from "./components/musica/musica.component";
+import { TelevisionComponent } from "./components/television/television.component";
+import { Error404Component } from "./components/error404/error404.component";
+
+//NECESITAMOS LOS SIGUIENTES MODULOS PARA REALIZAR ROUTING
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from "@angular/core";
+
+//DEFINIMOS UN ARRAY DE OBJETOS Routes
+const appRoutes: Routes = [
+    {path: "", component: HomeComponent},
+    {path: "musica", component: MusicaComponent},
+    {path: "cine", component: CineComponent},
+    {path: "television", component: TelevisionComponent},
+    {path: "**", component: Error404Component}
+]
+
+//DE ESTA CLASE DEBEMOS EXPORTAR DOS ELEMENTOS PARA app.module.ts
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = 
+RouterModule.forRoot(appRoutes);
+
+
+
+
+
